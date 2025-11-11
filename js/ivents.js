81b5463f724fcd0c377ec9fb6a42d71d659f3f28
PERed <danielredov123@gmail.com>
2025-11-11 17:22:33 +0300

    123

--- js/ivents.js
+++
@@ -1,9 +0,0 @@
-function redirectTo(site) {
-    const sites = {
-        'back': 'index.html',
-    };
-    
-    if (sites[site]) {
-        window.location.href = sites[site];
-    }
-}
--- js/test.js
+++
@@ -1,8 +0,0 @@
-// JS страница для теста всякой хуеты //
-    function verify_confirm() {
-        document.cookie="age_verification=1; path=/; SameSite=none; Secure";
-    }
-    function toggleButtonConfirm() {
-        document.querySelector(".verify_confirm").classList.toggle("button_disable");
-    }
-    
